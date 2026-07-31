8e4b3d1a11ba0081e1c5b569a29e3fe8c9de69c5
Tawsif Hossain <tawsifhossain18.o@gmail.com>
2026-07-31 14:40:01 +0600

    Done

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,21 @@
 import app from "./app";
+import config from "./config";
 import { prisma } from "./lib/prisma";
 
-const PORT = 3000;
+const PORT = config.port;
 
-async function main(){
-    try {
-                await prisma.$connect();
-        console.log("Connected to the database successfully.");
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
-    }catch(err){
-        console.error("Error starting the server:", err);
-        await prisma.$disconnect();
-        process.exit(1);
-    }
+async function main() {
+  try {
+    await prisma.$connect();
+    console.log("Connected to the database successfully.");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error("Error starting the server:", err);
+    await prisma.$disconnect();
+    process.exit(1);
+  }
 }
 
 main();
